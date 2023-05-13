9e7285722ee20715a89b7e76c250148c8e2d3ebc
Christopher Bülte <utinr@student.kit.edu>
2023-05-13 16:04:59 +0100

    update

--- tests/simulation_test.R
+++ tests/simulation_test.R
@@ -5,10 +5,12 @@ library(parallel)
 library(gridExtra)
 
 current_path = rstudioapi::getActiveDocumentContext()$path 
-setwd(dirname(current_path ))
+setwd(dirname(current_path))
+#Get nodes
+no_cores <- detectCores() - 2
 
 # Simulate Brown-Resnick model
-simulate_train <- function(params){
+simulate <- function(params){
   range <- params[["range"]]
   smooth <- params[["smooth"]]
   x <- seq(1,20, length = 25)
@@ -28,13 +30,12 @@ train_params <- cbind(range,smooth)
 #Save params
 save(train_params, file = "../data/data_test/train_params.RData")
 #Create train set
-no_cores <- detectCores() - 2
 # Initiate cluster
 cl <- makeCluster(no_cores)
 clusterExport(cl,c('simulate_train'))
 clusterEvalQ(cl, library(SpatialExtremes))
 
-train_data <- parApply(cl, train_params, MARGIN = 1, FUN = simulate_train)
+train_data <- parApply(cl, train_params, MARGIN = 1, FUN = simulate)
 stopCluster(cl)
 
 
@@ -44,13 +45,6 @@ save(train_data, file = "../data/data_test/train_data.RData")
 
 
 #Test set
-simulate <- function(n, range, smoothness){
-  x <- seq(1,20, length = 25)
-  coord <- cbind(x,x)
-  brown <- rmaxstab(n = n, coord = coord, cov.mod = "brown", range = range, smoothness = smoothness, grid = TRUE)
-  return(brown)
-}
-
 #Generate parameters
 n <- 50
 range_seq <- c(0.5, 0.75, 1, 1.5)
@@ -58,20 +52,20 @@ smooth_seq <- c(0.8,1.05,1.3,1.55)
 comb <- expand.grid(range_seq, smooth_seq)
 n_comb <- dim(comb)[1]
 test_params <- cbind(rep(comb$Var1, each = 50), rep(comb$Var2, each = 50))
+colnames(test_params) <- c("range", "smooth")
 #Save parameters
 save(test_params, file = "../data/data_test/test_params.RData")
 
+# Initiate cluster
+cl <- makeCluster(no_cores)
+clusterExport(cl,c('simulate_train'))
+clusterEvalQ(cl, library(SpatialExtremes))
 
-result <- array(data = 0, dim = c((n_comb),25,25,n))
+test_data <- parApply(cl, test_params, MARGIN = 1, FUN = simulate)
+stopCluster(cl)
 
-for (i in 1:n_comb){
-  range <- comb[i,1]
-  smooth <- comb[i,2]
-  sim <- simulate(n, range = range, smoothness = smooth)
-  result[i,,,] <- sim
-}
+save(test_data, file = "../data/data_test/test_data.RData")
 
-save(result, file = "../data/data_test/test_data.RData")
 
 
 
