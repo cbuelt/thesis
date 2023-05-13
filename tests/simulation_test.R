ac374d0b149eadb3adcd44c587140325a14065fc
Christopher Bülte <utinr@student.kit.edu>
2023-05-13 14:47:12 +0100

    update

--- tests/simulation_test.R
+++ tests/simulation_test.R
@@ -8,56 +8,38 @@ current_path = rstudioapi::getActiveDocumentContext()$path
 setwd(dirname(current_path ))
 
 # Simulate Brown-Resnick model
-simulate_train <- function(n, range_seq, smooth_seq){
+simulate_train <- function(params){
+  range <- params[["range"]]
+  smooth <- params[["smooth"]]
   x <- seq(1,20, length = 25)
   coord <- cbind(x,x)
-  range <- runif(1, min = range_seq[1], max = range_seq[2])
-  smooth <- runif(1, min = smooth_seq[1], max = smooth_seq[2])
-  brown <- rmaxstab(n = n, coord = coord, cov.mod = "brown", range = range, smoothness = smooth, grid = TRUE)
-  return(list(data = brown, range = range, smooth = smooth))
+  brown <- rmaxstab(n = 1, coord = coord, cov.mod = "brown", range = range, smoothness = smooth, grid = TRUE)
+  return(brown)
 }
+# Simulate training data
+n <- 2000
+range_seq = c(0.1,3)
+smooth_seq = c(0.5,1.9)
 
-
+# Simulate uniform range
+range <- runif(n, min = range_seq[1], max = range_seq[2])
+smooth <- runif(n, min = smooth_seq[1], max = smooth_seq[2])
+train_params <- cbind(range,smooth)
+#Save params
+save(train_params, file = "../data/data_test/train_params.RData")
 #Create train set
 no_cores <- detectCores() - 2
 # Initiate cluster
 cl <- makeCluster(no_cores)
+clusterExport(cl,c('simulate_train'))
 clusterEvalQ(cl, library(SpatialExtremes))
 
-n <- 2000
-train_set <- parLapply(cl, rep(1,n), simulate_train, range_seq = c(0.1,3), smooth_seq = c(0.5,1.9))
+train_data <- parApply(cl, train_params, MARGIN = 1, FUN = simulate_train)
 stopCluster(cl)
 
-# Create data outputs
-train_var <- data.frame(matrix(data = 0, nrow = n, ncol = 2))
-colnames(train_var) <- c("range", "smoothness")
-train_data <- data.frame(matrix(data = 0, nrow = n, ncol = 625))
-for (i in 1:n){
-  train_var[i,] <- c(train_set[[i]]$range, train_set[[i]]$smooth)
-  train_data[i,] <- array(train_set[[i]]$data)
-}
 
-write.csv(train_var, file = "sim_data/train_var.csv")
-write.csv(train_data, file = "sim_data/train_data.csv")
-
-
-
-x <- seq(1,20, length = 25)
-grid <- expand.grid(x=x, y=x)
-
-rand <- seq(1,4)
-grid$"a" <- array(train_set[[rand[1]]]$data)
-grid$"b" <- array(train_set[[rand[2]]]$data)
-grid$"c" <- array(train_set[[rand[3]]]$data)
-grid$"d" <- array(train_set[[rand[4]]]$data)
-
-
-x1 <- levelplot(a~x*y, grid, col.regions= terrain.colors(100), main = paste0(round(train_var[1,1],4),"-", round(train_var[1,2],4)))
-x2 <- levelplot(b~x*y, grid, col.regions= terrain.colors(100), main = paste0(round(train_var[2,1],4),"-", round(train_var[2,2],4)))
-x3 <- levelplot(c~x*y, grid, col.regions= terrain.colors(100), main = paste0(round(train_var[3,1],4),"-", round(train_var[3,2],4)))
-x4 <- levelplot(d~x*y, grid, col.regions= terrain.colors(100), main = paste0(round(train_var[4,1],4),"-", round(train_var[4,2],4)))
-
-grid.arrange(x1, x2, x3, x4, ncol=2)
+#Save train_data
+save(train_data, file = "../data/data_test/train_data.RData")
 
 
 
@@ -68,31 +50,28 @@ simulate <- function(n, range, smoothness){
   brown <- rmaxstab(n = n, coord = coord, cov.mod = "brown", range = range, smoothness = smoothness, grid = TRUE)
   return(brown)
 }
+
+#Generate parameters
+n <- 50
 range_seq <- c(0.5, 0.75, 1, 1.5)
 smooth_seq <- c(0.8,1.05,1.3,1.55)
 comb <- expand.grid(range_seq, smooth_seq)
-n_comb <- length(range_seq) * length(smooth_seq)
-n <- 50
-results <- data.frame(matrix(data = 0, nrow = (n_comb * n), ncol = (2+25**2)))
+n_comb <- dim(comb)[1]
+test_params <- cbind(rep(comb$Var1, each = 50), rep(comb$Var2, each = 50))
+#Save parameters
+save(test_params, file = "../data/data_test/test_params.RData")
+
+
+result <- array(data = 0, dim = c((n_comb),25,25,n))
+
 for (i in 1:n_comb){
   range <- comb[i,1]
   smooth <- comb[i,2]
   sim <- simulate(n, range = range, smoothness = smooth)
-  data <- as.matrix(data.frame(data = aperm(sim, c(3,1,2))))
-  results[(1+50*(i-1)):(50*i),] <- as.data.frame(cbind(rep(range, n), rep(smooth, n), data))
+  result[i,,,] <- sim
 }
 
-write.csv(results, file = "sim_data/test_data.csv")
-
-c <- 20
-x <- seq(1,20, length = 25)
-grid <- expand.grid(x=x, y=x)
-res <- t(results[c,3:627])
-grid$z <- res
-smooth <- results[c,1]
-range <- results[c,2]
-levelplot(z~x*y, grid, col.regions= terrain.colors(100), main = paste0(round(smooth,4),"-", round(range,4)))
-
+save(result, file = "../data/data_test/test_data.RData")
 
 
 
