c61f5965d8bc3c4cf4b56a8393aa0bdc6272938d
Chris Bülte <cbuelte@econ-stat-rr01.econ.kit.edu>
2023-07-11 11:25:04 +0200

    update

--- tests/simulate_data.R
+++ tests/simulate_data.R
@@ -29,13 +29,18 @@ simulate <- function(params){
 #
 
 # Set parameters
-n <- 2500
-exp <- "exp_4"
+n <- 1000
+exp <- "exp_4_1"
 
 # Simulate parameters
 smooth <- runif(n = n, min = 0, max = 1.5)
 range <- runif(n = n, min = 0.5, max = 3)
 train_params <- cbind(range, smooth)
+
+#Generate 5 pairs of parameters for aggregating
+n_pair <- 5
+train_params <- cbind(rep(range, each = n_pair), rep(smooth, each = n_pair))
+colnames(train_params) <- c("range", "smooth")
 
 for (model in c("brown", "powexp")){
   #Save params
@@ -56,23 +61,23 @@ for (model in c("brown", "powexp")){
 
 # Create test dataset
 # Set parameters
-n_each <- 50
+n_each <- 20 * n_pair
 n <- 25
-exp <- "exp_4"
+exp <- "exp_4_1"
 
 # Simulate parameters
 smooth <- rep(runif(n = n, min = 0, max = 1.5), each = n_each)
 range <- rep(runif(n = n, min = 0.5, max = 3), each = n_each)
 
-range <- c(0.2748737, 0.4687288, 3.575283, 4.846655, 1.362708, 1.525501, 4.017220, 3.238309)
-smooth<- c(0.0845058, 1.069622, 0.6416916, 0.3100938, 1.899021, 1.984236, 1.613812, 1.793770)
-range <- rep(range, each = n_each)
-smooth <- rep(smooth, each = n_each)
+#range <- c(0.2748737, 0.4687288, 3.575283, 4.846655, 1.362708, 1.525501, 4.017220, 3.238309)
+#smooth<- c(0.0845058, 1.069622, 0.6416916, 0.3100938, 1.899021, 1.984236, 1.613812, 1.793770)
+#range <- rep(range, each = n_each)
+#smooth <- rep(smooth, each = n_each)
 test_params <- cbind(range, smooth)
 
 for (model in c("brown", "powexp")){
   #Save params
-  save(test_params, file = paste0("../data/",exp,"/data/", model, "_test_params_outside.RData"))
+  save(test_params, file = paste0("../data/",exp,"/data/", model, "_test_params.RData"))
   #Create train set
   # Initiate cluster
   cl <- makeCluster(no_cores)
@@ -83,5 +88,5 @@ for (model in c("brown", "powexp")){
   stopCluster(cl)
   
   #Save train_data
-  save(test_data, file = paste0("../data/",exp,"/data/", model, "_test_data_outside.RData"))
+  save(test_data, file = paste0("../data/",exp,"/data/", model, "_test_data.RData"))
 }
