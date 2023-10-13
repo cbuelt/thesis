e37edb9534b15a177852ae24edcc048ea40ade55
Christopher <chrissie97@gmx.de>
2023-10-13 10:22:09 +0200

    update

--- abc/abc_method.R
+++ abc/abc_method.R
@@ -3,49 +3,40 @@ setwd(dirname(current_path))
 #Load functions
 source("abc_functions.R")
 #Get nodes
-no_cores <-25#detectCores() - 1
-
-
-
+no_cores <-detectCores() - 4
 
 
 
 #Steps
 #Define grid
-exp <- "exp_5"
-model <- "powexp"
-n <- 1
+exp <- "outside_model"
+model <- "smith"
+# Define grid
 length <- 30
-x <- seq(0, 10, length = length)
+x <- seq(0, length, length = length)
 grid <- expand.grid(x,x)
 grid <- array(unlist(grid), dim = c(length**2,2))
 
-
 # Define smaller grid for downsampling
 downsample_size <- 5
-x_small <- seq(0, 10, length = downsample_size)
+x_small <- seq(0, 30, length = downsample_size)
 grid_small <- expand.grid(x_small,x_small)
 grid <- array(unlist(grid_small), dim = c(downsample_size**2,2))
 
 #Load data
 load(paste0("../data/", exp,"/data/", model, "_test_data.RData"))
 load(paste0("../data/", exp,"/data/", model, "_test_params.RData"))
+model <- "brown"
 
 
 #Measure time
 start_time <- Sys.time()
 
-# Calculate distance matrix
-cluster_res <- get_clusters(grid = grid, n_stations = dim(grid)[1], method = "full")
-print("Time for distance matrix:")
-print(Sys.time()-start_time)
-
 #Run simulations
-index <- seq(1, 50, 1)
-result <- sapply(X = index, FUN = abc_wrapper, simplify = "array", grid, cluster_res, model, 50000, n_cores = no_cores, interp = "interpolate", n_sim_each = 25,
-                 q_filter = 0.01)
+index <- seq(1, 2, 1)
+result <- sapply(X = index, FUN = abc_wrapper, simplify = "array", grid, model, 50000, n_cores = no_cores, interp = "interpolate", n_sim_each = 25)
 #Save results
-save(result, file = paste0("../data/",exp,"/results/", model, "_abc_samples_interpolated_6.RData"))
+save(result, file = paste0("../data/",exp,"/results/", model, "_abc_results.RData"))
 print(dim(result))
 print(Sys.time()-start_time)
 
